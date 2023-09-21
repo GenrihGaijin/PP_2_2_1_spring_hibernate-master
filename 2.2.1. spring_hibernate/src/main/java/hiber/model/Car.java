30bf47b78a76c177fe1a0cf299f0dccc7102896a
Ivan Nikolaev <ivanix8@gmail.com>
2023-09-21 21:13:40 +0400

    add class Car

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -1,13 +1,33 @@
 package hiber.model;
 
 public class Car {
-    private String value;
+    private String model;
+private int series;
+private long id;
+public Car(){}
+    public Car(String model, int series){
+    this.model=model;
+    this.series=series;
+    }
+    public long getId(){
+    return id;
+    }
+    public void setId(long id){
+    this.id=id;
 
-    public String getValue() {
-        return value;
+    }
+    public String getModel() {
+        return model;
     }
 
-    public void setValue(String value) {
-        this.value = value;
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public int getSeries(){
+        return series;
+    }
+    public void setSeries(int series){
+        this.series=series;
+
     }
 }
